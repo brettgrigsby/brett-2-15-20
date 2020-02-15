6baf752210df00c817982c6b4be819ed60f027e6
brettgrigsby <gbrett@gmail.com>
2020-02-15 15:21:27 -0700

    remove space in presenter

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,6 +1,6 @@
 function prettySize(size) {
   if (size > 1000000) return Math.round(size / 1000000) + 'mb'
-  if (size > 1000) return Math.round(size / 1000) + ' kb'
+  if (size > 1000) return Math.round(size / 1000) + 'kb'
   return size + 'b'
 }
 
